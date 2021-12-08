5ba7d567aa7ba52aa64c0a2207a681897cf4eeab
TagKir <tagirkiky@gmail.com>
2021-12-08 21:53:03 +0300

    Main

--- Крестики_нолики.py
+++ Крестики_нолики.py
@@ -17,12 +17,20 @@ def play_game():
 	global board
 	word='X'
 	player1=input('Кто играет за крестик?\n')
+	while player1=='':
+		player1=input('Так кто играет за крестик!?\n')
 	player2=input('Кто играет за нолик?\n')
+	while player2=='':
+		player2=input('Так кто играет за нолик!?\n')
 	common_player=player1
 	print('Приятно с вами познакомиться,а теперь приступаем к игре!')
 	while winner(board)==0:
-		index=int(input(f'{common_player},где ты хочешь поставить' +(' крестик' if word=='X' else ' нолик')+ '\n'))
-		if board[index-1]!='X' and 'O': 
+		try:
+			index=int(input(f'{common_player},где ты хочешь поставить' +(' крестик' if word=='X' else ' нолик')+ '\n'))
+		except:
+			print('Нельзя писать не цифру')
+			continue
+		if board[index-1]!='X' and board[index-1]!='O': 
 			board[index-1]=word
 		else:
 			print('Писать в одно и тоже место нельзя!')
