6a3379ce895325555392963dcd8098fd7e7947c5
TagKir <tagirkiky@gmail.com>
2021-12-08 21:30:44 +0300

    Main

--- Крестики_нолики.py
+++ Крестики_нолики.py
@@ -5,6 +5,35 @@ def print_board():
 			print(f'{c}')
 		else:
 			print(f'{c}|', end='')
-print_board()
+
 winner_comb=[(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
-def winner():
+def winner(board):
+	for x, z, y in winner_comb:
+		if board[x]==board[z] and board[z]==board[y] and board[x]=='X' or board[x]==board[z] and board[z]==board[y] and board[x]==board[y] and board[x]=='O':
+			return ''
+		draw=[draw for draw in board if draw==' ']
+		if draw!=[]:
+			continue
+		else:
+			print('Ничья,продолжаем играть')
+			board=[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+	return 0
+
+def play_game():
+	word='X'
+	player1=input('Кто играет за крестик?\n')
+	player2=input('Кто играет за нолик?\n')
+	common_player=player1
+	print('Приятно с вами познакомиться,а теперь приступаем к игре!')
+	while winner(board)==0:
+		index=int(input(f'{common_player},где ты хочешь поставить' +(' крестик' if word=='X' else ' нолик')+ '\n'))
+		if board[index-1]!='X' and 'O': 
+			board[index-1]=word
+		else:
+			print('Писать в одно и тоже место нельзя!')
+			continue
+		print_board()
+		word='O' if word=='X' else 'X'
+		common_player=player2 if common_player==player1 else player1
+	print('Победитель:' + (player2 if common_player==player1 else player1))
+play_game()
