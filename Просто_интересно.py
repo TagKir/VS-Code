f4c73ada26d84ae3d0950b058c531053b6a247cf
TagKir <tagirkiky@gmail.com>
2022-06-14 21:32:46 +0300

    Main

--- Просто_интересно.py
+++ Просто_интересно.py
@@ -1,12 +1,10 @@
-def Fib(int):
-	x1 = 0
-	x2 = 0
-	x3 = 1
-	x4 = 0
-	while x1<int:
-		x1+=1
-		x4 = x2x4 = x2
-		x2+=x3
-		print(x2)	
-		x3 = x4
-Fib(5)
+def valid_ISBN10(isbn): 
+	ISBN = 100
+	if len(isbn)==10:
+		if isbn[-1]=='X': isbn = isbn[:-1]
+		else: ISBN-=100
+		ISBN += [isb*int(pos) for isb,pos in enumerate(isbn, start= 1)]
+		return True if ISBN % 11 == 0 else False
+	else:
+		return False
+print(valid_ISBN10('048665088X'))
